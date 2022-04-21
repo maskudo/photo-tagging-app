6ef9c27f2cb17e19d6a7467875573e5db2c20b4e
maskudo <manujkarki101@gmail.com>
2022-04-21 13:01:26 +0545

    get relative coordinates on clicking

--- src/Components/GameScreen.js
+++ src/Components/GameScreen.js
@@ -1,10 +1,17 @@
 import backgroundImg from "../Assets/Img/background.jpg"
 
 function GameScreen() {
+    const imgOnClick = (e) => {
+        const imgObj = document.getElementById("game-screen")
+        const startCoord = imgObj.getBoundingClientRect()
+        const x_coord = Math.round(e.pageX - startCoord.x)
+        const y_coord =  Math.round(e.pageY - startCoord.y)
+        console.log(x_coord,y_coord)
+    } 
     return (
-        <>
-            <img className="game-img" src={backgroundImg} alt="game-img" />
-        </>
+        <div id='game-screen'>
+            <img onClick={imgOnClick} className="game-img" id="game-img" src={backgroundImg} alt="game-img" />
+        </div>
     );
 }
 
