c11c64bc14b4955a04dba716799dde36ecad5052
maskudo <manujkarki101@gmail.com>
2022-04-24 23:03:29 +0545

    modify styles

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -3,18 +3,22 @@ import styled from "styled-components";
 const MenuContainer = styled.div`
     display:flex;
     flex-direction:column;
+    justify-content: space-between;
     overflow:hidden;
     background: rgba(0, 0, 0, 0.6);
     position: absolute;
     border: solid black 1px;
     border-radius: 15%;
+    min-height: 2rem;
+    min-width: 5rem;
     color:white;
     left: ${props => `${props.left}px`};
     top: ${props => `${props.top}px`};
     z-index: 5;
 `
 const Button = styled.div`
-    padding: 7%;
+    padding: 5%;
+    text-align: center;
     :hover{
         background: rgba(0, 0, 0, 0.8)
     }
