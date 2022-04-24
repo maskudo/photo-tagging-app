622fdf5f434f29af7644e03e832d4163cf35d63e
maskudo <manujkarki101@gmail.com>
2022-04-24 14:19:33 +0545

    display leaderboard

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import db from "./../firebase"
+import { db } from "../firebase";
 
 const getLeaderboard = async () => {
     const colRef = collection(db, 'leaderboard');
@@ -14,9 +14,39 @@ const getLeaderboard = async () => {
   }
 
 function Leaderboard() {
+    const [leaderboard, setLeaderboard] = useState("")
+    useEffect(() => {
+        const getLbd = async () => {
+            const data = await getLeaderboard()
+            setLeaderboard(data)
+        } 
+        getLbd()
+    }, [])
     return (
         <>
             <h1>leaderboard lol</h1>
+            {leaderboard && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Player</th>
+                            <th>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {leaderboard.slice(0,10).map((player, index) => {
+                            return (
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{player.name}</td>
+                                    <td>{('0'+(player.time-(player.time%60))/60).slice(-2)} : {('0' + (player.time%60)).slice(-2)}</td>
+                                </tr>
+                            )
+                        })} 
+                    </tbody>
+                   
+                </table>}
         </>
     );
 }
