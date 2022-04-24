0060c28589d7d964e31b5c1350e83cdcc7314214
maskudo <manujkarki101@gmail.com>
2022-04-24 19:16:55 +0545

    add styles to component

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,27 +1,37 @@
 import styled from "styled-components";
 
-const Title = styled.div`
-        display:flex;
-        flex-direction:column;
-        position: absolute;
-        color:white;
-        left: ${props => `${props.left}px`};
-        top: ${props => `${props.top}px`};
-        z-index: 5;
-    `
+const MenuContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    overflow:hidden;
+    background: rgba(0, 0, 0, 0.6);
+    position: absolute;
+    border: solid black 1px;
+    border-radius: 15%;
+    color:white;
+    left: ${props => `${props.left}px`};
+    top: ${props => `${props.top}px`};
+    z-index: 5;
+`
+const Button = styled.div`
+    padding: 7%;
+    :hover{
+        background: rgba(0, 0, 0, 0.8)
+    }
+`
 
 function Dropdown(props) {
     const {left, top, characters, buttonOnClick} = props
     return (
-        <Title left={left} top={top} >
+        <MenuContainer left={left} top={top} >
             {characters.map((character) => {
                 return (
                     <>
-                        <button onClick={buttonOnClick}>{character}</button>
+                        <Button onClick={buttonOnClick}>{character}</Button>
                     </>
                 )
             })}
-        </Title>
+        </MenuContainer>
     );
 }
 
