ff1ad84f2a4981bfb3a68173d0755edea1a032df
maskudo <manujkarki101@gmail.com>
2022-04-24 13:37:37 +0545

    add function to get leaderboard from backend

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import db from "./../firebase"
 
+const getLeaderboard = async () => {
+    const colRef = collection(db, 'leaderboard');
+    let data = []
+    const querySnapshot = await getDocs(colRef);
+    querySnapshot.docs.forEach((doc) => {
+        data.push({...doc.data(), id:doc.id})
+    })
+    data.sort((a,b) => {return (a.time-b.time)})
+    return data
+  }
 
 function Leaderboard() {
     return (
