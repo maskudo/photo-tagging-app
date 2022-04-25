2d301f5ade7b833d3c9e3dc0dd461aebed851b20
maskudo <manujkarki101@gmail.com>
2022-04-25 13:32:20 +0545

    add styles and disable button after click

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,18 +6,50 @@ import Leaderboard from "./Leaderboard"
 
 const PopupForm = styled.form`
     display: ${props => `${props.display}`};
+    text-align: start;
     flex-direction: column;
+    justify-content: space-between;
     position: fixed;
-    top: 20%;
-    left: 20%;
+    border: solid black 1px;
+    background: rgba(0,0,0,0.5);
+    padding: 2%;
+    top: 30%;
+    left: 40%;
     z-index:5;
+    color: #e8e6e3;
+`
+const Input = styled.input`
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    border: none;
+    background: rgb(32, 35, 36);
+    color:#e8e6e3;
+    font-size: 1.5rem;
+
+    :focus{
+        background: rgb(25,30,30);
+        outline:none;
+    }
+`
+const Button = styled.button`
+    font-size: 1.5rem;
+    background-color: #04AA6D;
+    color: white;
+    padding: 16px 20px;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    margin-bottom:10px;
+    opacity: 0.8;
 `
 function Form(props) {
     const [name, setName] = useState("")
     const [submitted, setSubmitted] = useState(false)
+    const [disabled, setDisabled] = useState(false)
     const OnSubmit = (e) => {
         e.preventDefault()
-        console.log(name)
+        setDisabled(true)
         const colRef = collection(db, "leaderboard")
         addDoc(colRef, {
             name: name,
@@ -30,8 +62,9 @@ function Form(props) {
     return (
         <>
             <PopupForm display={submitted?"none":"flex"}>
-                <input type="text" onChange={(e) => setName(e.target.value)} id="name" placeholder="Enter your name" required minLength={3}/>
-                <button type="submit" onClick={OnSubmit}>Submit</button>
+                <label htmlFor="name">Submit score</label>
+                <Input type="text" onChange={(e) => setName(e.target.value)} id="name" placeholder="Enter Name" required minLength={3}/>
+                <Button type="submit" disabled={disabled} onClick={OnSubmit}>Submit</Button>
             </PopupForm>
             {submitted && <Leaderboard currentPlayer = {{
                 name: name,
